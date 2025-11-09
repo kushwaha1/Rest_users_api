1dd2768f50136ba36999b7cbdc39cf810666b200
Sweety <rsweet375@gmail.com>
2025-11-10 00:34:58 +0530

    setup initial project, install express, port listen and add users array

---
+++ src/data/users.js
@@ -0,0 +1,72 @@
+export const users = [
+    {
+        id: 1,
+        firstName: 'Erica',
+        lastName: 'Mathews',
+        age: 25,
+        hobby: 'Teaching'
+    },
+    {
+        id: 2,
+        firstName: 'Rajat',
+        lastName: 'Kumar',
+        age: 28,
+        hobby: 'Photography'
+    },
+    {
+        id: 3,
+        firstName: 'Michael',
+        lastName: 'Davis',
+        age: 32,
+        hobby: 'Gardening'
+    },
+    {
+        id: 4,
+        firstName: 'Sarah',
+        lastName: 'Wilson',
+        age: 27,
+        hobby: 'Cooking'
+    },
+    {
+        id: 5,
+        firstName: 'James',
+        lastName: 'Brown',
+        age: 31,
+        hobby: 'Guitar Playing'
+    },
+    {
+        id: 6,
+        firstName: 'Lisa',
+        lastName: 'Anderson',
+        age: 29,
+        hobby: 'Reading'
+    },
+    {
+        id: 7,
+        firstName: 'David',
+        lastName: 'Taylor',
+        age: 34,
+        hobby: 'Hiking'
+    },
+    {
+        id: 8,
+        firstName: 'Jessica',
+        lastName: 'Martinez',
+        age: 26,
+        hobby: 'Dancing'
+    },
+    {
+        id: 9,
+        firstName: 'William',
+        lastName: 'Thompson',
+        age: 33,
+        hobby: 'Chess'
+    },
+    {
+        id: 10,
+        firstName: 'Emily',
+        lastName: 'Garcia',
+        age: 30,
+        hobby: 'Yoga'
+    }
+]
